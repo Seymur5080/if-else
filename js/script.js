de485e797b139a74438e1c3b14a2387ebcff7f67
seymurquluzade <60833025+Seymur5080@users.noreply.github.com>
2021-11-24 11:32:33 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,43 @@
+/*
+let y = document.getElementById("inp").value;
+let say = document.getElementById("div");
+let i = 666;
+
+
+function hesab() {
+   if (i <= "665") {
+      alert("Reqem yazdiginizdan yuxaridadi");
+   } else if (i >= "667" ) {
+      alert("Reqem yazdiginizdan awaqidadi");
+   } else if (i == "666") {
+      alert("Duzdur");
+   } else {
+      alert('Uduzduz');
+   }
+}
+
+hesab();
+*/
+
+
+
+
+let say = prompt('0-1000 arasinda bir reqem secin', '');
+
+
+if (say <= 665) {
+   alert('Reqem yazdiginizdan yuxaridadi');
+} else if (say >= 667 ) {
+   alert('Reqem yazdiginizdan awaqidadi');
+} else if (say == 666) {
+   alert("Udduz");
+} else {
+   alert('Uduzduz');
+}
+
+
+
+
+
+
+
